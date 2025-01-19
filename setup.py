05faf92adf820ec1de0a7f18885be1988fe34aed
Rahul <rahul@gmail.com>
2025-01-19 22:05:34 +0545

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='AI powered chatbot',
+    version='0.0.0',
+    author='Rahul Shah, Kohinoor Shrestha',
+    author_email='rahul.shah@deerwalk.edu.np, kohinoor.shrestha@deerwalk.edu.np',
+    packages= find_packages(),
+    install_requires= []
+)
